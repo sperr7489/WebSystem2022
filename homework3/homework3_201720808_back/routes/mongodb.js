f738c2da030c378bb02c736239f7194e4cdee5f6
sperr <sperr@ajou.ac.kr>
2022-12-05 12:08:42 +0900

    fix : homework3  db 이름 수정

--- homework3/homework3_201720808_back/routes/mongodb.js
+++ homework3/homework3_201720808_back/routes/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  await mongoose.connect("mongodb://localhost:27017/bibliography", {
+  await mongoose.connect("mongodb://localhost:27017/homework3_201720808", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
